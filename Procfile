5457a345514e1f19f5874c65dd118c1c427265af
O'Brian Kim <kimo@Brians-MacBook-Pro.local>
2016-11-19 20:10:17 +0300

    (Feature /Chore) Postgres Integration / Dependendy refactor

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bucketlistAPI.budcketlistAPI.wsgi
--- bucketlistAPI/bucketlistAPI/settings.py
+++ bucketlistAPI/bucketlistAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -90,12 +91,29 @@ WSGI_APPLICATION = 'bucketlistAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('DJANGO_BL_DB'),
+#         'USER': os.getenv('DJANGO_BL_USER'),
+#         'PASSWORD': os.getenv('DJANGO_BL_PASS'),
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
 # Password validation
