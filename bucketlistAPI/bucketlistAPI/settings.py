0407c448962b9d0838523b6f389f17991ac5f9cd
O'Brian Kim <kimo@Brians-MacBook-Pro.local>
2016-11-24 06:37:11 +0300

    (Chore) Update requirements.txt

--- bucketlistAPI/bucketlistAPI/settings.py
+++ bucketlistAPI/bucketlistAPI/settings.py
@@ -93,26 +93,26 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-# DATABASES = {
-    # 'default': dj_database_url.config()
 #Sqlite Configuration
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 # posgresql configuration
+# Sqlite Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DJANGO_BL_DB'],
-        'USER': os.environ['DJANGO_BL_USER'],
-        'PASSWORD': os.environ['DJANGO_BL_PASS'],
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# posgresql configuration
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ['DJANGO_BL_DB'],
+#         'USER': os.environ['DJANGO_BL_USER'],
+#         'PASSWORD': os.environ['DJANGO_BL_PASS'],
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
